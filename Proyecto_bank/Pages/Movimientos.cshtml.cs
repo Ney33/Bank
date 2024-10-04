32619f0867e28fbe5560019f40e783790a2590fa
juan lopez <juanxj93@gmail.com>
2024-10-04 16:47:08 -0500

    primer paso

---
+++ Proyecto_bank/Pages/Movimientos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Proyecto_bank.Pages
+{
+    public class MovimientosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
